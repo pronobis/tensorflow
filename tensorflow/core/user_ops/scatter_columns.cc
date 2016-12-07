741e99fe127abb7eb6c8fba9b8e5901388adc3c0
Avinash Ranganath <nash911@gmail.com>
2016-12-07 01:49:41 +0100

    Add custom op scatter_columns()

---
+++ tensorflow/core/user_ops/scatter_columns.cc
@@ -0,0 +1,199 @@
+#include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/framework/op_kernel.h"
+#include "tensorflow/core/framework/register_types.h"
+#include "tensorflow/core/platform/prefetch.h"
+#include "../kernels/bounds_check.h" //--TODO: Currently <#include "tensorflow/core/kernels/bounds_check.h"> gives an error. Need to check and fix.
+#include "scatter_columns_functor.h"
+
+using namespace tensorflow;
+
+REGISTER_OP("ScatterColumns")
+.Input("params: T")
+.Input("indices: IndT")
+.Input("out_num_cols: IndT")
+.Input("pad_elem: T")
+.Output("columns: T")
+.Attr("T: type")
+.Attr("IndT: {int32,int64}");
+
+template <typename Device, typename T, typename IndT>
+class ScatterColumnsOp : public OpKernel {
+public:
+  explicit ScatterColumnsOp(OpKernelConstruction* ctx) : OpKernel(ctx) {
+    const DataType data_t = DataTypeToEnum<T>::v();
+    const DataType index_t = DataTypeToEnum<IndT>::v();
+    OP_REQUIRES_OK(ctx, ctx->MatchSignature({data_t, index_t, index_t, data_t}, {data_t}));
+  }
+
+  void Compute(OpKernelContext* ctx) override {
+
+    //--Grab the input tensor - params--//
+    const Tensor& params = ctx->input(0);
+
+    //--Grab the input tensor - indices--//
+    const Tensor& indices = ctx->input(1);
+    auto indices_flat = indices.flat<IndT>();
+
+    //--Grab the input - out_num_cols--//
+    const Tensor& out_num_cols_tensor = ctx->input(2);
+
+    //--Grab the input - pad_elem--//
+    const Tensor& pad_elem_tensor = ctx->input(3);
+
+    //--Check and convert out_num_cols into scalar--//
+    IndT out_num_cols = out_num_cols_tensor.scalar<IndT>()();
+
+    //--Check and convert pad_elem into scalar--//
+    T pad_elem = pad_elem_tensor.scalar<T>()();
+
+    OP_REQUIRES(ctx, TensorShapeUtils::IsVectorOrHigher(params.shape()),
+                errors::InvalidArgument("params must be at least a vector"));
+
+    OP_REQUIRES(ctx, TensorShapeUtils::IsVector(indices.shape()),
+                errors::InvalidArgument("indices must be a vector, but it is a: ", indices.dims(), "D Tensor."));
+
+    int64 indices_size = indices.dim_size(0);
+
+    OP_REQUIRES(ctx, indices_size > 0,
+                errors::InvalidArgument("indices cannot be empty."));
+
+    const TensorShape& params_shape(params.shape());
+
+    OP_REQUIRES(ctx, params_shape.dims() <= 2,
+                errors::InvalidArgument("params must be 1D or 2D but it is: ", params_shape.dims(), "D"));
+
+
+    TensorShape output_shape(params_shape);
+
+    int64 params_rows;
+    int64 params_cols;
+
+    if(params_shape.dims() == 1)
+    {
+      params_rows = 1;
+      params_cols = params.dim_size(0);
+
+      //--Set output tensor dims--//
+      output_shape.set_dim(0, out_num_cols);
+    }
+    else if(params_shape.dims() == 2)
+    {
+      params_rows = params.dim_size(0);
+      params_cols = params.dim_size(1);
+
+      //--Set output tensor dims--//
+      output_shape.set_dim(0, params_rows);
+      output_shape.set_dim(1, out_num_cols);
+    }
+
+    OP_REQUIRES(ctx, out_num_cols >= params_cols,
+                errors::InvalidArgument("out_num_cols: ", out_num_cols,
+                                        " must be >= size of the indexed dimension of params: ", params_cols));
+
+    OP_REQUIRES(ctx, indices_size == params_cols,
+                errors::InvalidArgument("Size of indices: ", indices_size,
+                                        " and the indexed dimension of params - ", params_cols, " - must be the same."));
+
+    unordered_set<IndT> unique_ind(&indices_flat(0), &indices_flat(indices_size));
+
+    OP_REQUIRES(ctx, unique_ind.size() == indices_size,
+                errors::InvalidArgument("indices cannot contain duplicates.",
+                                        " Total no. of indices: ", indices_size,
+                                        " != no. of unique indices: ", unique_ind.size()));
+
+    //--Arrange output indices--//
+    std::vector<IndT> out_indices(out_num_cols, -1); //--Here '-1' refers to padding column(s)--//
+    for(IndT i=0; i<indices_size; i++)
+    {
+      //--Check indices[i] ∈ (0, out_num_cols]--//
+      OP_REQUIRES(ctx, FastBoundsCheck(indices_flat(i), out_num_cols),
+                  errors::InvalidArgument("indices(", i, "): ", indices_flat(i), " is not in range (0, ", out_num_cols, "]."));
+
+      out_indices[indices_flat(i)] = i;
+    }
+
+    //--Group consecutive padding columns together--//
+    //--E.g.:  params = [11, 12, 13, 14]
+    //-- out_num_cols = 10
+    //--     pad_elem = 0
+    //--      indices = [7, 4, 2, 3]
+    //--      output  = [0, 0, 13, 14, 12, 0, 0, 11, 0, 0]
+    //--cons_pad_cols = [2, 1, 0, 0, 0, 2, 1, 0, 2, 1]
+
+    std::vector<int> cons_pad_cols(out_num_cols, 0);
+    int pad_cols;
+    int max_cons_pad_cols = 0;
+
+    for(int c = 0; c < out_num_cols; c++)
+    {
+      pad_cols = 0;
+      while(out_indices[c + pad_cols] < 0)
+      {
+        pad_cols++;
+        if(c + pad_cols >= out_num_cols)
+        {
+          break;
+        }
+      }
+
+      if(pad_cols > max_cons_pad_cols)
+      {
+        max_cons_pad_cols = pad_cols;
+      }
+
+      while(pad_cols > 0)
+      {
+        cons_pad_cols[c++] = pad_cols--;
+      }
+    }
+
+    //--Vector containing padding elements. Size of this vector = maximum no. of consecutive padding columns in the output tensor--//
+    gtl::InlinedVector<T, 4> pad_elem_vec(max_cons_pad_cols, pad_elem);
+
+    //--Create an output tensor--//
+    Tensor* output = NULL;
+    OP_REQUIRES_OK(ctx, ctx->allocate_output(0, output_shape, &output));
+
+    auto output_tensor = output->shaped<T, 2>({params_rows, out_num_cols});
+    auto params_tensor = params.shaped<T, 2>({params_rows, params_cols});
+
+    functor::ScatterColumnsFunctor<Device, T, IndT> functor;
+    int64 bad_i = functor(ctx->eigen_device<Device>(),
+                          params_tensor,
+                          out_num_cols,
+                          out_indices,
+                          cons_pad_cols,
+                          pad_elem_vec,
+                          params_rows,
+                          output_tensor);
+
+    OP_REQUIRES(ctx, bad_i < 0,
+                errors::InvalidArgument("bad_i: ", bad_i));
+  }
+};
+
+
+#define REGISTER_SCATTERCOLUMNS_INT32(type) \
+  REGISTER_KERNEL_BUILDER(Name("ScatterColumns") \
+  .Device(DEVICE_CPU) \
+  .TypeConstraint<type>("T") \
+  .TypeConstraint<int32>("IndT"), \
+  ScatterColumnsOp<CPUDevice, type, int32>)
+
+TF_CALL_ALL_TYPES(REGISTER_SCATTERCOLUMNS_INT32);
+
+#undef REGISTER_SCATTERCOLUMNS_INT32
+
+
+#define REGISTER_SCATTERCOLUMNS_INT64(type) \
+  REGISTER_KERNEL_BUILDER(Name("ScatterColumns") \
+  .Device(DEVICE_CPU) \
+  .TypeConstraint<type>("T") \
+  .TypeConstraint<int64>("IndT"), \
+  ScatterColumnsOp<CPUDevice, type, int64>)
+
+TF_CALL_ALL_TYPES(REGISTER_SCATTERCOLUMNS_INT64);
+
+#undef REGISTER_SCATTERCOLUMNS_INT64
+
+
---
+++ tensorflow/core/user_ops/scatter_columns_op_test.py
@@ -0,0 +1,198 @@
+
+import unittest
+import tensorflow as tf
+import numpy as np
+
+
+class TestMath(unittest.TestCase):
+    def test_scatter_cols_errors(self):
+        # TODO
+        pass
+
+    scatter_columns_module = tf.load_op_library('./scatter_columns.so')
+
+    def test_scatter_cols(self):
+        def test(params, indices, num_cols, pad_elem, dtype, true_output):
+
+            with self.subTest(params=params, indices=indices,
+                              num_cols=num_cols, pad_elem=pad_elem,
+                              dtype=dtype):
+                if dtype == bool:
+                    row1 = row2 = row3 = 1
+                else:
+                    row1 = 1
+                    row2 = 0
+                    row3 = -1
+
+                p1d = tf.constant(params, dtype=dtype)
+                p2d1 = tf.constant(np.array([np.array(params)]), dtype=dtype)
+                p2d2 = tf.constant(np.array([np.array(params) * row1,
+                                             np.array(params) * row2,
+                                             np.array(params) * row3]), dtype=dtype)
+
+                op1d = self.scatter_columns_module.scatter_columns(p1d, indices, num_cols, pad_elem)
+                op2d1 = self.scatter_columns_module.scatter_columns(p2d1, indices, num_cols, pad_elem)
+                op2d2 = self.scatter_columns_module.scatter_columns(p2d2, indices, num_cols, pad_elem)
+
+                with tf.Session() as sess:
+                    out1d = sess.run(op1d)
+                    out2d1 = sess.run(op2d1)
+                    out2d2 = sess.run(op2d2)
+
+                np.testing.assert_array_almost_equal(out1d, true_output)
+                self.assertEqual(dtype.as_numpy_dtype, out1d.dtype)
+
+
+                true_output_2d1 = [np.array(true_output)]
+                np.testing.assert_array_almost_equal(out2d1, true_output_2d1)
+                self.assertEqual(dtype.as_numpy_dtype, out2d1.dtype)
+
+                r_1 = np.array(true_output)
+                r_2 = np.array(true_output)
+                r_3 = np.array(true_output)
+                ind = np.array(indices)
+
+                r_1[ind] = r_1[ind] * row1
+                r_2[ind] = r_2[ind] * row2
+                r_3[ind] = r_3[ind] * row3
+
+                true_output_2d2 = [r_1,
+                                   r_2,
+                                   r_3]
+                np.testing.assert_array_almost_equal(out2d2, true_output_2d2)
+                self.assertEqual(dtype.as_numpy_dtype, out2d2.dtype)
+
+
+        float_val = 1.23456789
+        int_val = 123456789
+        int_32_upper = 2147483647
+        int_64_upper = 9223372036854775807
+
+        pad_elem = 333
+
+        # Single column output
+        # float
+        test([float_val],
+             [0],
+             1,
+             pad_elem,
+             tf.float32,
+             [float_val])
+        test([float_val],
+             [0],
+             1,
+             pad_elem,
+             tf.float64,
+             [float_val])
+
+        # int
+        test([int_32_upper],
+             [0],
+             1,
+             pad_elem,
+             tf.int32,
+             [int_32_upper])
+        test([int_64_upper],
+             [0],
+             1,
+             pad_elem,
+             tf.int64,
+             [int_64_upper])
+
+        # bool
+        test([True],
+             [0],
+             1,
+             False,
+             tf.bool,
+             [True])
+
+        # Multi-column output, single-column input
+        test([float_val],
+             [1],
+             4,
+             pad_elem,
+             tf.float32,
+             [pad_elem, float_val, pad_elem, pad_elem])
+        test([float_val],
+             [0],
+             4,
+             pad_elem,
+             tf.float64,
+             [float_val, pad_elem, pad_elem, pad_elem])
+
+        # int
+        test([int_32_upper],
+             [2],
+             5,
+             pad_elem,
+             tf.int32,
+             [pad_elem, pad_elem, int_32_upper, pad_elem, pad_elem])
+        test([int_64_upper],
+             [4],
+             5,
+             pad_elem,
+             tf.int64,
+             [pad_elem, pad_elem, pad_elem, pad_elem, int_64_upper])
+
+        # bool
+        test([True],
+             [3],
+             5,
+             False,
+             tf.bool,
+             [False, False, False, True, False])
+
+        # Multi-column output, multi-column input
+        # float
+        # No consecutive padded columns
+        test([float_val, float_val*2, float_val*3, float_val*4, float_val*5, float_val*6],
+             [5, 3, 9, 1, 8, 6],
+             10,
+             pad_elem,
+             tf.float32,
+             [pad_elem, float_val*4, pad_elem, float_val*2, pad_elem, float_val, float_val*6, pad_elem, float_val*5, float_val*3])
+        # Consecutive padded columns in the end
+        test([float_val, float_val*2, float_val*3, float_val*4, float_val*5, float_val*6],
+             [5, 3, 9, 1, 8, 6],
+             15,
+             pad_elem,
+             tf.float64,
+             [pad_elem, float_val*4, pad_elem, float_val*2, pad_elem, float_val, float_val*6, pad_elem, float_val*5, float_val*3, pad_elem, pad_elem, pad_elem, pad_elem, pad_elem])
+
+        # int
+        # Consecutive padded columns in the beginning
+        test([int_val, int_val*2, int_val*3, int_val*4, int_val*5, int_val*6, int_val*7, int_val*8, int_val*9],
+             [7, 14, 5, 9, 10, 11, 6, 3, 8],
+             15,
+             pad_elem,
+             tf.int32,
+             [pad_elem, pad_elem, pad_elem, int_val*8, pad_elem, int_val*3, int_val*7, int_val, int_val*9, int_val*4, int_val*5, int_val*6, pad_elem, pad_elem, int_val*2])
+        # Consecutive padded columns in the middle
+        test([int_val, int_val*2, int_val*3, int_val*4, int_val*5, int_val*6, int_val*7, int_val*8, int_val*9],
+             [13, 8, 4, 1, 2, 3, 11, 0, 9],
+             15,
+             pad_elem,
+             tf.int64,
+             [int_val*8, int_val*4, int_val*5, int_val*6, int_val*3, pad_elem, pad_elem, pad_elem, int_val*2, int_val*9, pad_elem, int_val*7, pad_elem, int_val, pad_elem])
+
+        # bool
+        # No padded columns
+        test([True, False, False, True],
+             [2, 1, 3, 0],
+             4,
+             False,
+             tf.bool,
+             [True, False, True, False])
+        # Consecutive padded columns in the beginning, middle and end
+        test([True, False, False, True],
+             [5, 11, 3, 9],
+             15,
+             False,
+             tf.bool,
+             [False, False, False, False, False, True, False, False, False, True, False, False, False, False, False])
+
+
+
+if __name__ == '__main__':
+    unittest.main()
